060852207afc2f20236815beaf417c7422ea434e
KacpersDev <vkacperpl@gmail.com>
2024-07-03 02:05:46 +0200

    code clean & create class

--- crates/mongo/src/lib.rs
+++ crates/mongo/src/lib.rs
@@ -6,19 +6,67 @@ pub struct Student {
     pub credit: u32,
 }
 
-pub async fn create_student(student: Student) -> mongodb::error::Result<()> {
-    let client = Client::with_uri_str("mongodb://localhost:27017/").await?;
+pub struct Teacher {
+    pub name: String,
+    pub class: String,
+    pub subject: String,
+}
+
+pub struct Class {
+    pub name: String,
+    pub students: Vec<&'static str>,
+}
+
+pub enum EntityType {
+    STUDENT(Student),
+    TEACHER(Teacher)
+}
+
+pub async fn create_entity(entity: EntityType) -> mongodb::error::Result<()> {
+
+    let client = Client::with_uri_str("uri").await?;
     let database = client.database("school");
-    let student_collection: Collection<Document> = database.collection("students");
+
+    match entity {
+        EntityType::STUDENT(student) => {
+            let student_collection: Collection<Document> = database.collection("students");
+
+            let insert_document = doc! {
+                "name": student.name,
+                "class": student.class,
+                "credit": student.credit,
+            };
+
+            student_collection.insert_one(insert_document).await?;
+        }
+        EntityType::TEACHER(teacher) => {
+            let teacher_collection: Collection<Document> = database.collection("teachers");
+
+            let insert_document = doc! {
+                "name": teacher.name,
+                "class": teacher.class,
+                "subject": teacher.subject,
+            };
+
+            teacher_collection.insert_one(insert_document).await?;
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn create_class(class: Class) -> mongodb::error::Result<()> {
+
+    let client = Client::with_uri_str("uri").await?;
+    let database = client.database("classes");
+    let classes_collection: Collection<Document> = database.collection("classes");
 
     let insert_document = doc! {
-        "name": student.name,
-        "class": student.class,
-        "credit": student.credit,
+        "name": class.name,
+        "students": class.students,
     };
 
-    let result = student_collection.insert_one(insert_document).await?;
-    println!("result id {}", result.inserted_id);
+    classes_collection.insert_one(insert_document).await?;
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use mongo::{create_student, Student};
-
-fn main() {
-    let student = Student{
-        name: String::from("Kacper"),
-        class: String::from("IT"),
-        credit: 0,
-    };
-
-    let rt = tokio::runtime::Runtime::new().unwrap();
-    rt.block_on(create_student(student)).expect("Error has occured while creating a student.");
+fn main(){
+    print!("Hello World");
 }
